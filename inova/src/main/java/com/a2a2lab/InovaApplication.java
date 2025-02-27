f367ef8e6154f83dc78fbfa75f64e380a6390fa8
tdj4909 <tdj4909@gmail.com>
2025-02-27 15:42:32 +0900

    update

---
+++ inova/src/main/java/com/a2a2lab/InovaApplication.java
@@ -0,0 +1,13 @@
+package com.a2a2lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovaApplication.class, args);
+	}
+
+}
